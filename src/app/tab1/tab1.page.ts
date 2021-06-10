649be11d7ff31e06edb7a1c12181e8dbe2f57c86
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2021-06-10 14:01:34 -0300

    alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,121 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public nomeTab = 'Filmes favoritos';
+  public buscarTexto: string;
+
+  public listaDeFilmes = [
+    {
+      imagem : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bzDAfXoqNAvWUe7uss2NE3BmRqy.jpg',
+      nome: 'Soul: Uma Aventura com Alma (2020)',
+      lancamento: '25/12/2020',
+      duracao: '1h 40m',
+      generos: ['Animação', 'Comédia', 'Família', 'Aventura'],
+      avaliacao: '83%'
+    },
+    {
+      imagem : 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/Am1ipOQiOMrH55tCCT2ObNiF1rW.jpg',
+      nome: 'Monster Hunter (2020)',
+      lancamento: '25/02/2021',
+      duracao: '1h 43m',
+      generos: ['Ação', 'Fantasia', 'Aventura','Ação', 'Fantasia', 'Aventura'],
+      avaliacao: '70%'
+    }
+  ];
+
+  constructor(
+    public alertController: AlertController
+  ) {}
+
+  async presentAlertCheckbox() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Checkbox',
+      inputs: [
+        {
+          name: 'checkbox1',
+          type: 'checkbox',
+          label: 'Checkbox 1',
+          value: 'value1',
+          handler: () => {
+            console.log('Checkbox 1 selected');
+          },
+          checked: true
+        },
+
+        {
+          name: 'checkbox2',
+          type: 'checkbox',
+          label: 'Checkbox 2',
+          value: 'value2',
+          handler: () => {
+            console.log('Checkbox 2 selected');
+          }
+        },
+
+        {
+          name: 'checkbox3',
+          type: 'checkbox',
+          label: 'Checkbox 3',
+          value: 'value3',
+          handler: () => {
+            console.log('Checkbox 3 selected');
+          }
+        },
+
+        {
+          name: 'checkbox4',
+          type: 'checkbox',
+          label: 'Checkbox 4',
+          value: 'value4',
+          handler: () => {
+            console.log('Checkbox 4 selected');
+          }
+        },
+
+        {
+          name: 'checkbox5',
+          type: 'checkbox',
+          label: 'Checkbox 5',
+          value: 'value5',
+          handler: () => {
+            console.log('Checkbox 5 selected');
+          }
+        },
+
+        {
+          name: 'checkbox6',
+          type: 'checkbox',
+          label: 'Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6',
+          value: 'value6',
+          handler: () => {
+            console.log('Checkbox 6 selected');
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  buscar(element: any): void{
+    const textBusca = element.detail.value;
+    console.log(textBusca);
+  }
 
 }
